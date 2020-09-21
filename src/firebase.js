03cc1178f6182c27f3d9d6501f7aacf5145bff83
John Knotts <jhnnyk@gmail.com>
2020-09-21 14:35:43 -0600

    set up firebase (hosting only)

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCgaYfPgw3DF6nyKEWqfKzuhUXgwd1rEw0',
+  authDomain: 'jhnnyk-personal.firebaseapp.com',
+  databaseURL: 'https://jhnnyk-personal.firebaseio.com',
+  projectId: 'jhnnyk-personal',
+  storageBucket: 'jhnnyk-personal.appspot.com',
+  messagingSenderId: '840292978678',
+  appId: '1:840292978678:web:9a89f399b98685be0c41d7',
+  measurementId: 'G-LXL1DBW8DM',
+};
+
+firebase.initializeApp(firebaseConfig);
