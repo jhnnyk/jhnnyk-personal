7f50a61bd9239c547d51648e64d5feef5e18a009
John Knotts <jhnnyk@gmail.com>
2020-09-22 11:29:46 -0600

    add results

---
+++ src/pages/ResultsPage.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="results">
+    <h1>Results</h1>
+    <h3>2020</h3>
+    <ul>
+      <li>August 26 - Ruby Creek Traverse FKT - 3:01:14</li>
+      <li>
+        May 31 - Lost Creek Wilderness South Loops (Big Loop) FKT - 8:01:23
+      </li>
+    </ul>
+    <h3>2019</h3>
+    <ul>
+      <li><strong>August 23 - Divide 100k - 1st - 12:59:20</strong></li>
+      <li><s>July 19 - Hardrock 100</s> cancelled due to snow</li>
+      <li>
+        <strong
+          >June 29 - Western States 100 - 41st Overall, 29th Male -
+          20:49:15</strong
+        >
+      </li>
+      <li><strong>March 9 - Running Up for Air Colorado - 1st</strong></li>
+    </ul>
+
+    <h3>2018</h3>
+    <ul>
+      <li>
+        <!-- TODO -->
+        <strong
+          >Jan 3-Dec 24 -
+          <a href="https://www.jhnnyk.com/posts/10"
+            >100 Different Mountains</a
+          ></strong
+        >
+      </li>
+      <li>September 14 - Run Rabbit Run 100: 23rd (hares) - 26:33:28</li>
+      <li>June 23 - San Juan Solstice 50: 27th - 10:47:22</li>
+      <li>March 24 - Behind the Rocks 50: 23rd - 10:17:19</li>
+    </ul>
+
+    <h3>2017</h3>
+    <ul>
+      <li><strong>September 1 - Nolans 14: 57:55:00</strong></li>
+      <li><strong>July 14 - Hardrock 100: 35th - 34:14:58</strong></li>
+    </ul>
+
+    <h3>2016</h3>
+    <ul>
+      <li>August 26 - Nolans 14: DNF</li>
+      <li>June 17 - Bighorn 100: 19th - 23:19:12</li>
+      <li>April 2 - Behind the Rocks 50: 10th - 9:12:08</li>
+    </ul>
+
+    <h3>2015</h3>
+    <ul>
+      <li>September 25 - The Bear 100: 51st - 26:47:08</li>
+      <li>May 30 - Golden Gate Dirty 30: 28th - 5:42:17</li>
+      <li>March 7 - Way Too Cool 50k: 76th - 4:19:57</li>
+    </ul>
+
+    <h3>2014</h3>
+    <ul>
+      <li>September 13 - Run Rabbit Run 100: 24th (hares) - 28:01:36</li>
+      <li>June 21 - San Juan Solstice 50: 29th - 11:21:19</li>
+      <li>May 31 - Golden Gate Dirty 30: 78th - 7:09:28</li>
+      <li>April 6 - Zion 100k: 12th - 12:39:20</li>
+    </ul>
+
+    <h3>2013</h3>
+    <ul>
+      <li>September 28 - UROC 100k: 58th - 16:15:27</li>
+      <li>July 27 - Siskiyou Out Back 50: 42nd - 10:15:04</li>
+      <li>June 9 - Dirty Half Marathon: 25th - 1:32:42</li>
+      <li>April 13 - Lake Sonoma 50: 124th - 10:16:59</li>
+      <li>February 24 - Montara Mountain 50k: 12th - 5:54:31</li>
+    </ul>
+
+    <h3>2012</h3>
+    <ul>
+      <li>December 1 - TNF San Francisco 50: 156th - 9:48:32</li>
+      <li>September 22 - Flagline 50k: 33rd - 5:13:07</li>
+      <li>August 5 - Haulin' Aspen Trail Marathon: 11th - 3:41:16</li>
+      <li>June 10 - Dirty Half Marathon: 85th - 1:38:24</li>
+    </ul>
+
+    <h3>2010</h3>
+    <ul>
+      <li>June 13 - Dirty Half Marathon: 226th - 1:52:52</li>
+    </ul>
+
+    <h3>2007</h3>
+    <ul>
+      <li>August 12 - Haulin' Aspen Trail Marathon: 19th - 3:47:16</li>
+      <li>April 15 - Peterson Ridge Rumble 30k: 35th - 2:44:44</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultsPage',
+};
+</script>
