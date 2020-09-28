320cb74da3a5c88f906c50507f265d52a0334717
John Knotts <jhnnyk@gmail.com>
2020-09-28 07:18:44 -0600

    add homepage content

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="homepage">
     <h1>Home</h1>
-    <p>Welcome to the personal website of John Knotts.</p>
+    <p>
+      Welcome to the personal website of John Knotts.
+    </p>
+    <img src="../assets/family.png" alt="my crew" />
+    <p>
+      I'm a stay-at-home dad and I love my wife and kids. They’re amazing people
+      and so much fun to be around — my crew for life. If I'm not with them I'm
+      usually running in the mountains or dabbling in software development.
+    </p>
   </div>
 </template>
 
@@ -10,3 +18,9 @@ export default {
   name: 'HomePage',
 };
 </script>
+
+<style>
+.homepage {
+  text-align: center;
+}
+</style>
